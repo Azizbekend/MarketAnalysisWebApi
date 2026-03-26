3947acfead92bf568b919091f7992de4f823086c
NacharVS <60977438+NacharVS@users.noreply.github.com>
2026-03-26 14:09:49 +0300

    Update DbPumpConfiguration.cs

--- MarketAnalysisWebApi/DbEntities/DbRequestConfigurations/DbPumpConfiguration.cs
+++ MarketAnalysisWebApi/DbEntities/DbRequestConfigurations/DbPumpConfiguration.cs
@@ -1,4 +1,6 @@
-﻿namespace MarketAnalysisWebApi.DbEntities.DbRequestConfigurations
+﻿using MarketAnalysisWebApi.DbEntities.Base;
+
+namespace MarketAnalysisWebApi.DbEntities.DbRequestConfigurations
 {
     public enum LiquidType
     {
@@ -6,7 +8,14 @@
         FacilityLiquids,
         RainIceLiquids
     }
-    public class DbPumpConfiguration
+
+    public enum LiquidsIntakeType
+    {
+        Even = 1,
+        Periodic,
+        Uneven
+    }
+    public class DbPumpConfiguration : DbBase
     {
         public LiquidType PumpedLiquidType { get; set; }
         public double PumpEfficiency { get; set; }
@@ -15,5 +24,17 @@
         public double MineralParticlesConcentration { get; set; }
         public bool BigParticleExistance { get; set; }
         public string? SpecificWastes { get; set; } // если есть специфические отходы, прописать через запятую
+        public double LiquidDensity  { get; set; }
+        public bool ExplosionProtection { get; set; }
+        public string? ControlType { get; set; }
+        public string? PowerCurrentType { get; set; }
+        public double WorkPower { get; set; }
+        public bool FrequencyConverter { get; set; }
+        public double PowerCableLength { get; set; }
+        public bool LiftingTransportEquipment { get; set; }
+        public bool FlushValve { get; set; }
+        public bool OtherLevelMeters { get; set; }
+        public string? OtherRequirements { get; set; }
+
     }
 }
