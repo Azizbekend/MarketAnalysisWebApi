3342ee26df7b2bb93c92253edbdfd654ee20dda5
NacharVS <60977438+NacharVS@users.noreply.github.com>
2026-04-01 08:02:14 +0300

    новые файлы миграций

---
+++ MarketAnalysisWebApi/Migrations/20260331102953_InstalationPlaceAddedInKNS.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketAnalysisWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InstalationPlaceAddedInKNS : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InstalationPlace",
+                table: "KnsConfigurations",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InstalationPlace",
+                table: "KnsConfigurations");
+        }
+    }
+}
