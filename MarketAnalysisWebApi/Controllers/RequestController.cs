dd0988a0ad0fe44bcc13a2dd7b7a583dc17dba61
NacharVS <60977438+NacharVS@users.noreply.github.com>
2026-04-02 14:53:29 +0300

    Update RequestController.cs

--- MarketAnalysisWebApi/Controllers/RequestController.cs
+++ MarketAnalysisWebApi/Controllers/RequestController.cs
@@ -132,17 +132,18 @@ namespace MarketAnalysisWebApi.Controllers
             var res = await _projectRequestRepo.GetFavoriteProjectRequestsByUserIdAsync(id);
             return Ok(res);
         }
-        //public async Task<IActionResult> GetUsersFavouritesRequests(Guid userId)
-        //{
-        //    var res = await _projectRequestRepo.GetFavourites(userId);
-        //    return Ok(res);
-        //}
-        //[HttpGet("user/favourites")]
-        //public async Task<IActionResult> GetUsersFavouritesRequests(Guid userId)
-        //{
-        //    var res = await _projectRequestRepo.GetFavourites(userId);
-        //    return Ok(res); 
-        //}
+        [HttpGet("favorites/user")]
+        public async Task<IActionResult> GetUsersFavouritesRequests(Guid userId)
+        {
+            var res = await _projectRequestRepo.GetFavourites(userId);
+            return Ok(res);
+        }
+        [HttpGet("user/favourites")]
+        public async Task<IActionResult> GetUsersFavouritesRequests(Guid userId)
+        {
+            var res = await _projectRequestRepo.GetFavourites(userId);
+            return Ok(res);
+        }
         [HttpPost("favourites/add")]
         public async Task<IActionResult> AddToFavourites(RequestStandartDTO dto)
         {
