b5f91988557a4900a1e9cc18371f840268f9a741
NacharVS <60977438+NacharVS@users.noreply.github.com>
2026-04-03 16:23:23 +0300

    долил

---
+++ MarketAnalysisWebApi/DTOs/NotificationsDTO/NotificationDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace MarketAnalysisWebApi.DTOs.NotificationsDTO
+{
+    public class NotificationDTO
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public string Type { get; set; } // "RequestCreated", "OfferReceived"
+        public DateTime CreatedAt { get; set; }
+        public object Data { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
